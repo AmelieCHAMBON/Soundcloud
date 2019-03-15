a6a180fab20ab10f9b954d0285d16329362e647e
samuel.mohamedanli <samuel_anlihely@ens.univ-artois.fr>
2019-03-15 11:17:34 +0100

    update style + ajout de tag div dans view _chansons.blade et dans app.blade

--- resources/views/_chansons.blade.php
+++ resources/views/_chansons.blade.php
@@ -1,5 +1,7 @@
-<ul>
+<div id="groupeChansons">
+<ul class="wrapper">
         @foreach($chansons as $c)
     <li><a class="chanson" data-file='{{$c->fichier}}' href="#">{{$c->nom}}</a> par l'utilisateur <a href="/utilisateur/{{$c->utilisateur->id}}">{{$c->utilisateur->name}}</a></li>
         @endforeach
     </ul>
+</div>
